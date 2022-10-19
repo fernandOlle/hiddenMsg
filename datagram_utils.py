9e7681a673e7bb8fa2af26166970d1386a4c1467
Fernando Ollé <55040050+fernandOlle@users.noreply.github.com>
2022-10-18 21:35:33 -0300

    Add files via upload

--- datagram_utils.py
+++ datagram_utils.py
@@ -1,67 +1,91 @@
-from rsa import *
+from typing import Dict
+import socket
+import sys
+from xmlrpc.client import boolean
+import rsa
 from random import randint
+import json
+import os
 
+publickey = None
+privatekey = None
+their_pbkey = None
 
-pub_key = None
-priv_key = None
-
-their_pub_key = None
-
-ori = None
-sh = None
-
-header = {
-    "orientation": ori,
-    "shuffle": sh,
-    "ciphertext": "4spk63eAxsnI/B5ErUNSCJwN+9X0Eclmqck/zy3o7Iu/aRxPyxUytjUrI0rD2e4="
-}
-
-
-def desencapsulate(datagram: header):
+def desencapsulate(datagram: Dict):
     # desempacota a message
     sh = datagram["shuffle"]
     ori = datagram["orientation"]
     string = datagram["ciphertext"]
+    end = datagram["end"]
 
+    if end:
+        return "/END"
+
+    new_string = ''
     if ori:
-        new_string = None
         for char in string:
-            new_string = ord(char)
-            hodler += -sh
-            new_string = chr(new_string)
-        return new_string
+            holder = ord(char) + sh
+            new_string += chr(holder)
     else:
         new_string = string[-sh:]
         new_string += string[:-sh]
-        print(new_string)
-        return new_string
+    return new_string
 
 
-def encapsulate(string):
+def encapsulate(string: str, endChat: boolean):
     # empacota a message
-    sh = randint(a=0, b=50)
+    sh = randint(a=0, b=len(string) - 1)
     ori = bool(randint(a=0, b=1))
 
+    if endChat:
+        return {
+            "orientation": ori,
+            "shuffle": sh,
+            "end": True,
+            "ciphertext": ""
+        }
+
+    new_string = ''
     if ori:
-        new_string = None
         for char in string:
-            new_string = ord(char)
-            hodler += sh
-            new_string = chr(new_string)
-        return new_string
+            holder = ord(char) - sh
+            new_string += chr(holder)
     else:
         new_string = string[sh:]
         new_string += string[:sh]
-        print(new_string)
-        return new_string
+
+    return {
+            "orientation": ori,
+            "shuffle": sh,
+            "end": False,
+            "ciphertext": new_string
+        }
 
 
-def handshake(s):
-    # gera criptografia
-    pub_key, priv_key = rsa.newKeys(512)
+def receive(s: socket.socket):
+    while True:
+        r_msg = s.recv(1024).decode()
+        r_msg = json.loads(r_msg)
+        r_msg = desencapsulate(r_msg)
+        if r_msg == '':
+            pass
+        if r_msg == '/END':
+            print("finalizando conversa...")
+            os._exit(0)
+        else:
+            print('\n' + r_msg)
 
-    key = encapsulate(pub_key)
-    s.sendall(key)
-    their_pub_key = s.recv(1024)
-    their_pub_key = desencapsulate(their_pub_key)
-    print(their_pub_key + '\n' + pub_key + '\n' + priv_key)
+
+def send(s: socket.socket):
+    while True:
+        s_msg = input()#.replace('b', '')
+        if s_msg == '':
+            pass
+        if s_msg == '/END':
+            s_msg = encapsulate(s_msg, True)
+            s.sendall(json.dumps(s_msg).encode())
+            print("finalizando conversa...")
+            os._exit(0)
+        else:
+            s_msg = encapsulate(s_msg, False)
+            s.sendall(json.dumps(s_msg).encode())
--- server.py
+++ server.py
@@ -1,37 +1,8 @@
 import threading
 import socket
+import rsa
 from random import randint
 from datagram_utils import *
-
-
-def datagram():
-    sh = randint(a=0, b=10)
-    ori = bool(randint(a=0, b=1))
-
-    header = {
-        "orientation": ori,
-        "shuffle": sh,
-        "ciphertext": "4spk63eAxsnI/B5ErUNSCJwN+9X0Eclmqck/zy3o7Iu/aRxPyxUytjUrI0rD2e4="
-    }
-
-
-def connect(conn):
-    while True:
-        received = conn.recv(1024)
-        if received == ' ':
-            pass
-        else:
-            print('\n' + received.decode())
-
-
-def sendMsg(conn):
-    while True:
-        send_msg = input().replace('b', '').encode()
-        if send_msg == ' ':
-            pass
-        else:
-            conn.sendall(send_msg)
-
 
 if __name__ == '__main__':
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -39,9 +10,8 @@ if __name__ == '__main__':
     s.bind(('', 9090))
     s.listen()
     (conn, addr) = s.accept()
-    handshake(s)
-    thread1 = threading.Thread(target=connect, args=([conn]))
-    thread2 = threading.Thread(target=sendMsg, args=([conn]))
+    thread1 = threading.Thread(target=receive, args=([conn]))
+    thread2 = threading.Thread(target=send, args=([conn]))
     thread1.start()
     thread2.start()
     thread1.join()
